afbef39aa39b7df1a492d4f71f1299df56ea7179
Dmitriy Boiko <dmitriy1boiko@gmail.com>
2023-08-25 12:27:13 +0300

    task-5

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const ctrl = require('../../controllers/auth')
 
-const {validateBody, authenticate} = require('../../middelwares');
+const {validateBody, authenticate, upload} = require('../../middelwares');
 
 const {schemas} = require('../../models/user')
 
@@ -14,4 +14,7 @@ router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 router.get("/current", authenticate, ctrl.getCurrent);
 
 router.post("/logout", authenticate, ctrl.logout);
+
+router.patch("/avatars", authenticate, upload.single("avatar"), ctrl.updateAvatar)
+
 module.exports = router;
